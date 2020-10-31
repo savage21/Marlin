37865bf37b15e485c0b8964f5af30dcf4603186f
savage21 <djboxe@web.de>
2020-10-31 16:10:28 +0100

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1835,7 +1835,7 @@
 #if BOTH(SDSUPPORT, DIRECT_STEPPING)
   #define BLOCK_BUFFER_SIZE  8
 #elif ENABLED(SDSUPPORT)
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE 32                                                              //bearbeitet 16/32
 #else
   #define BLOCK_BUFFER_SIZE 16
 #endif
@@ -1844,7 +1844,7 @@
 
 // The ASCII buffer for serial input
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 32                                                                          //bearbeitet 4/32
+#define BUFSIZE 64                                                                          //bearbeitet 4/32
 
 // Transmission to Host Buffer Size
 // To save 386 bytes of PROGMEM (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
